9b850df0d0d2b085ad203be799b7147abcc5fb2f
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-09 13:03:08 -0500

    connect to databse mongoDB

---
+++ db/connect.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const MongoClient = require("mongodb").MongoClient;
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log("The database is now initialized!");
+    return callback(null, _db);
+  }
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error("Uninitialized database");
+  }
+  return _db;
+};
+
+module.exports = { initDb, getDb };
